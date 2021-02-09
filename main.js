c30eac01ccd2a38236556d4d9a14f89a152cb746
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-02-08 20:45:05 -0600

    created profile section details and some js arrays

---
+++ main.js
@@ -0,0 +1,32 @@
+// Start arrays
+const organizations = [
+  {
+    img:"images/orgImgs/oi_nss.png",
+    name:"nss-evening-cohort-14",
+    repos:30,
+  },
+  {
+    img:"images/orgImgs/oi_org1.png",
+    name:"React Ladies",
+    repos:32,
+  },
+  {
+    img:"images/orgImgs/oi_org2.png",
+    name:"TN Code Pros",
+    repos:20,
+  },
+  {
+    img:"images/orgImgs/oi_org3.png",
+    name:"Fortune 500 Devs",
+    repos:27,
+  },
+];
+
+//Stretch:
+// Sponsors go on bottom of Profile section
+// const sponsors = [
+//   {
+
+//   },
+// ]
+// End arrays
