28ec32dc0d998a6dff084448ffed7aae5b44dac2
Sean <jb@JBs-MacBook-Pro.local>
2021-02-10 13:45:25 -0600

    merged and pushed up branch

--- main.js
+++ main.js
@@ -44,7 +44,6 @@ const organizations = [
   },
 ];
 
-<<<<<<< HEAD
 // Array of package objects
 const packages = [
   {
@@ -84,7 +83,6 @@ const packages = [
    id: 5,
    },
 ];
-=======
 const repos = [
   {
     name: 'example-repo',
@@ -100,7 +98,6 @@ const repos = [
 // Sponsors go on bottom of Profile section
 // const sponsors = [
 //   {
->>>>>>> 30492d801e8aea8b5839d8d02930e07f1a1b716f
 
 // HTML string of Package cards to be printed to DOM
 const packageCardString = (item) => { 
@@ -187,40 +184,12 @@ const profileString = `<!-- Profile -->
   <!-- Print Images of Organizations Object Here -->
   <div></div>
 <!-- Sponsors -->
-<<<<<<< HEAD
   <!-- Print Images of Sponsors Object Here -->`
 
-
-  
 // Init function
 const init = () => {
   printToDom("#profile-card", profileString);
   createCards(packages, packageCardString, "#package-container");
 }
-=======
-  <!-- Print Images of Sponsors Object Here -->`;
-  
-// end my code
-
-
-// Gabby - projects page 
-const projectCards = (projects) => {
-  return `<div class="card">
-  <h5 class="card-header"></h5>
-  <div class="card-body">
-    <h5 class="card-title">${projects.name}</h5>
-    <p class="card-text">${projects.description}</p>
-  </div>
-</div>`
-};
-
-
-
-// end projects page 
-const init = () => {
-  printToDom("#profile-card", profileString)
-  createCards(projects, projectCards, '#project-container');
-};
->>>>>>> 30492d801e8aea8b5839d8d02930e07f1a1b716f
 
 init();
