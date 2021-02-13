8569dc11cd0d182088341720a44afa09cfed4887
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-02-13 12:42:27 -0600

    fixed text color and indentation

--- main.js
+++ main.js
@@ -380,11 +380,11 @@ const projectsForm = () => {
     <div class='form-text'>Coordinate, track and update all in one place, so projects stay transparent and on schedule</div>
     <hr/>
     <label for="projectsForm" class="form-label mt-2 fw-bold new-project">Project board name</label>
-    <input type="text" class="form-control bg-transparent border border-secondary w-50" id="project-board-name" aria-describedby="projectBoardNameHelp" placeholder="Example" required>
+    <input type="text" class="form-control bg-transparent border border-secondary w-50 text-secondary" id="project-board-name" aria-describedby="projectBoardNameHelp" placeholder="Example" required>
   </div>
   <div class="mb-3">
     <label for="projectDescription" class="form-label fw-bold new-project">Description (optional)</label>
-    <input type="text" class="form-control bg-transparent border border-secondary pb-5 ps-5" id="project-description">
+    <input type="text" class="form-control bg-transparent border border-secondary pb-5 text-secondary" id="project-description">
   </div>
   <button type="submit" class="btn btn-success">Create project</button>
 </form>`;
