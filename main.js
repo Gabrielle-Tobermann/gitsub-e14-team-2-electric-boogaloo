2a5328d8b87d617601bcb46a78b1f7dc42e71288
HunterJuneau <huntlee17@gmail.com>
2021-02-13 09:42:40 -0600

    Fixed missing borders and form hidden behind footer.

--- main.js
+++ main.js
@@ -312,7 +312,7 @@ const profileString = `<!-- Profile -->
 <!-- Sponsors -->
   <!-- Print Images of Sponsors Object Here -->`;
 
-const repoCard = (item) => `<div class="repo-card w-100" style="width: 18rem;">
+const repoCard = (item) => `<div class="repo-card w-100 bottom-border" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${item.name}</h5>
       <p class="card-text">${item.description}</p>
