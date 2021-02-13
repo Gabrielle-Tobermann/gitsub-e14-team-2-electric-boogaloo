ccf9a40bbe8451315b0f2044032b7d347d73b20c
HunterJuneau <huntlee17@gmail.com>
2021-02-13 10:24:54 -0600

    Repo form requires name for submit.

--- main.js
+++ main.js
@@ -343,8 +343,8 @@ const repoFormSubmit = (e) => {
 
 const repoEvents = () => {
   document
-    .querySelector('#repoSubmit')
-    .addEventListener('click', repoFormSubmit);
+    .querySelector('#repoForm')
+    .addEventListener('submit', repoFormSubmit);
 };
 
 // End Create Profile Card
