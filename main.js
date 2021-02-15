cff7a6a280c86358eda2274080216045c9d7fa1b
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-02-14 22:56:16 -0600

    still working on sort functionality

--- main.js
+++ main.js
@@ -501,13 +501,12 @@ const projectsFormInfo = (e) => {
   createCards(projects, projectCards, '#project-container');
   document.querySelector('form').reset();
   document.querySelector('#sort-btn').addEventListener('click', sortProjectCards);
-};
 
+};
 //Gabby stretch goal - sort cards 
 const sortProjectCards = (e) => {
-
   if (e.target.id === 'sort-btn') {
-      sortedProjects = projects.slice().sort((a, b) => b.date - a.date);
+    sortedProjects = projects.sort((a, b) => b.date - a.date);
   };
   createCards(sortedProjects, projectCards, '#project-container');
 };
